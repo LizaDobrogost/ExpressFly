c6d8700368abca0a659b57489eca036212a7e8d4
Liza <Lizadobrogost@outlook.com>
2021-01-11 10:22:42 +0300

    DI for services and repose

---
+++ Backend/WebAPI/BusinessLogic/BusinessLogicModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessLogic.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLogic
+{
+    public class BusinessLogicModule
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddSingleton<ICountryService, CountryService>();
+        }
+    }
+}
---
+++ Backend/WebAPI/DataAccess/DataAccessModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess
+{
+    public static class DataAccessModule
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddSingleton<ICountryRepository, CountryRepository>();
+        }
+    }
+}
