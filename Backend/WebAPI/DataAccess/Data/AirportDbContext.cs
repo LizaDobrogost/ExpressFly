57e522c531551f1e497df00e893c76660f267b50
Liza <Lizadobrogost@outlook.com>
2021-01-11 10:24:28 +0300

    DbContext for EF

---
+++ Backend/WebAPI/DataAccess/Data/AirportDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class AirportDbContext : DbContext
+    {
+        public AirportDbContext(DbContextOptions<AirportDbContext> options) :
+            base(options)
+        {
+        }
+    }
+}
