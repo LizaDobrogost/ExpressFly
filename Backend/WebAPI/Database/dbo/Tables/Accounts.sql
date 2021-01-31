af780f2b4595058ca7dfc1cf2f96be436d8e8071
lizadobrogost@outlook.com <Lizadobrogost2000>
2021-02-01 02:42:31 +0300

    Add models for account

---
+++ Backend/WebAPI/Database/dbo/Tables/Accounts.sql
@@ -0,0 +1,9 @@
+create table [dbo].[Accounts] (
+    [Id]             int             identity (1, 1) not null,
+    [FirstName]      nvarchar (50)   not null,
+    [SecondName]     nvarchar (50)   not null,
+    [Email]          nvarchar (50)   not null,
+    [Password]		 nvarchar (255),
+    [Role]           int             not null,
+    constraint [PK_Accounts] primary key clustered ([Id] asc)
+);
