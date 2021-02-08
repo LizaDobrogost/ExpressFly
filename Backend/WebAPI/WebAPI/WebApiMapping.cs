3bda1e667477bcd104a2cb838d924908e23d9eff
lizadobrogost@outlook.com <Lizadobrogost2000>
2021-02-08 15:48:02 +0300

    Web Api changes

--- Backend/WebAPI/WebAPI/WebApiMapping.cs
+++ Backend/WebAPI/WebAPI/WebApiMapping.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using BusinessLogic.BusinessLogicModels;
 using BusinessLogic.Models;
-using WebApi.Models;
+using WebAPI.Models;
 using CountryModel = BusinessLogic.Models.CountryModel;
 
 namespace WebApi
@@ -13,8 +14,38 @@ namespace WebApi
     {
         public static void Initialize(IMapperConfigurationExpression config)
         {
-            config.CreateMap<CountryDto, CountryModel>();
-            config.CreateMap<CountryModel, CountryDto>();
+            config.CreateMap<City, CityModel>();
+            config.CreateMap<CityModel, City>();
+
+            config.CreateMap<Country,CountryModel>();
+            config.CreateMap<CountryModel, Country>();
+
+            config.CreateMap<Airport,AirportModel>();
+            config.CreateMap<AirportModel, Airport>();
+
+            config.CreateMap<Account, AccountModels>();
+            config.CreateMap<AccountModels, Account>();
+            config.CreateMap<AccountRequest, AccountModels>();
+
+            config.CreateMap<Airplane, AirplaneModel>();
+            config.CreateMap<AirplaneModel, Airplane>();
+            config.CreateMap<AirplaneFilter, AirplaneFilter>();
+            config.CreateMap<AirplaneSeat, AirplaneSeatModel>();
+            config.CreateMap<AirplaneSeatModel, AirplaneSeat>();
+            config.CreateMap<AirplaneSeatType, AirplaneSeatTypeModel>();
+            config.CreateMap<AirplaneSeatTypeModel, AirplaneSeatType>();
+            config.CreateMap<BusinessLogic.BusinessLogicModels.ItemsPage<Airplane>, WebAPI.Models.ItemsPage<Airplane>>();
+
+            config.CreateMap<Flight, FlightModel>();
+            config.CreateMap<FlightModel, Flight>();
+            config.CreateMap<FlightSeatTypeCost, FlightSeatTypeCostModel>();
+            config.CreateMap<FlightSeatTypeCostModel, FlightSeatTypeCost>();
+            config.CreateMap<FlightFilter, FlightFilterModel>();
+            config.CreateMap<FlightFilterModel, FlightFilter>();
+            config.CreateMap<FlightBookInfoModel, FlightBookInfo>();
+            config.CreateMap<SeatBook, SeatBookModel>();
+            config.CreateMap<SeatBookModel, SeatBook>();
+            config.CreateMap<BusinessLogic.BusinessLogicModels.ItemsPage<FlightModel>, WebAPI.Models.ItemsPage<Flight>>();
         }
     }
 }
